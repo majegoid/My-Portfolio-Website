5160d303c56786c3ce38decb9790a3c9f1221f0e
majegoid <majegoid@gmail.com>
2023-03-06 21:13:51 -0600

    refactors ProjectsList

--- components/Projects/ProjectsList/ProjectsList.jsx
+++ components/Projects/ProjectsList/ProjectsList.jsx
@@ -1,30 +1,9 @@
-import React, { useState } from 'react';
-import Project from '../Project/Project';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 
-import { container, projectList, viewButton } from './ProjectsList.module.css';
+import { projectList } from './ProjectsList.module.css';
 
 const ProjectsList = ({ children }) => {
-  const [areMainProjectsVisible, setAreMainProjectsVisible] = useState(true);
-
-  const toggleMainProjectsVisible = () => {
-    setAreMainProjectsVisible((curState) => !curState);
-  };
-
-  const toggleMainProjectsButton = (
-    <button className={viewButton} onClick={toggleMainProjectsVisible}>
-      <span>{areMainProjectsVisible ? 'Hide Main Projects' : 'View Main Projects'}</span>
-      <FontAwesomeIcon icon={areMainProjectsVisible ? 'eye-slash' : 'eye'} />
-    </button>
-  );
-
-  return (
-    <div className={container}>
-      {toggleMainProjectsButton}
-      {areMainProjectsVisible && <div className={projectList}>{children}</div>}
-      {areMainProjectsVisible && toggleMainProjectsButton}
-    </div>
-  );
+  return <div className={projectList}>{children}</div>;
 };
 
 export default ProjectsList;
